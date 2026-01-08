ad3e20a062294d1194e43ceea6185a6768384f10
Yulianaa7 <yuliananurhanifah77@gmail.com>
2026-01-08 19:20:43 +0700

    fix bug

--- js/data.js
+++ js/data.js
@@ -49,7 +49,7 @@ const anggotaKelompok = [
         nama: "Lely Sylva Jemima Tarigan",
         nim: "1202230034",
         role: "Anggota",
-        foto: "assets/images/Lely.jpeg",
+        foto: "assets/images/lely2.jpeg",
         bio: "Information Technology students at Telkom University Surabaya who are interested in learning English and improving their writing skills through academic projects and collaborations.",
         email: "lelysjtrg@student.telkomuniversity.ac.id"
     }
@@ -277,7 +277,7 @@ const artikelData = [
         </div>
     `,
         slideUrl: "assets/slides/lia.pdf",
-        slideTitle: "AI Tools Presentation",
+        slideTitle: "Digital Transformation as the Key to Business Success in the Modern Era",
         slideType: "pdf",
         slideThumbnail: "assets/images/digital-transformation-business.png",
         annotatedPdfUrl: "assets/annotated/lia.pdf",
